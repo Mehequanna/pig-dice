d1be176a408b363691f606a2e729b5fa0392536b
J. Ryan Conklin and Stephen Emery <jryanconklin@gmail.com>
2016-07-11 19:57:07 -0700

    Add functionality to revert score back to 0 if a 1 is rolled.

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,43 @@ var dieRoll = function() {
   return 1 + Math.floor(Math.random() * 6);
 }
 
-function Player(name, totalScore, turn) {
+function Player(name, totalScore, turn, runningTally) {
   this.name = name;
-  this.score = totalScore;
+  this.totalScore = totalScore;
   this.turn = turn;
+  this.runningTally = runningTally;
 }
+
 
 
 // Front End Logic
 
 $(document).ready(function(){
-  var player1 = new Player("Player 1", 0, 0);
-  var player2 = new Player("Player 2", 0, 0);
+  var player1 = new Player("Player 1", 0, 0, 0);
+  var player2 = new Player("Player 2", 0, 0, 0);
 
   $('#player1roll').click(function() {
-    $('#player1Active').html(dieRoll());
+    // var roll = dieRoll();
+    if (dieRoll() === 1) {
+      player1.runningTally = 0;
+      $('#player1Active').html(player1.runningTally);
+    } else {
+      player1.runningTally += dieRoll();
+      $('#player1Active').html(player1.runningTally);
+    }
   }); //End Player 1 Click Event
+
+
+
+
   $('#player2roll').click(function() {
-    $('#player2Active').html(dieRoll());
+    if (dieRoll() === 1) {
+      player2.runningTally = 0;
+      $('#player2Active').html(player2.runningTally);
+    } else {
+      player2.runningTally += dieRoll();
+      $('#player2Active').html(player2.runningTally);
+    }
   }); //End Player 1 Click Event
 
 
