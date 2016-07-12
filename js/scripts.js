2db7faa302e0a9fd3a2d8e223c4b044beab94f4c
J. Ryan Conklin and Stephen Emery <jryanconklin@gmail.com>
2016-07-11 19:23:41 -0700

    Add ability to print roll to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
+// Back-End Logic
 
-function dieRoll() {
+var dieRoll = function() {
   return 1 + Math.floor(Math.random() * 6);
 }
+
+
+
+// Front End Logic
+
+$(document).ready(function(){
+  $('#player1roll').click(function() {
+    $('#player1Active').html(dieRoll());
+  }); //End Player 1 Click Event
+  $('#player2roll').click(function() {
+    $('#player2Active').html(dieRoll());
+  }); //End Player 1 Click Event
+
+
+
+}); //End Document.Ready Function
